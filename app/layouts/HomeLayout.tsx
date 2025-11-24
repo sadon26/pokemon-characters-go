df3ced4da69c483f42be5db88cc38e25563b10ce
Dayo Ashaolu <dayo.ashaolu@tractionapps.co>
2025-11-24 08:19:50 +0100

    fixed header implementation

--- app/layouts/HomeLayout.tsx
+++ app/layouts/HomeLayout.tsx
@@ -15,7 +15,7 @@ const HomeLayout: FC<Props> = ({
   return (
     <div className="max-w-[800px] mx-auto px-6 flex flex-col h-screen">
       <Header />
-      <div className="grow overflow-y-scroll hide-scrollbar">
+      <div className="grow overflow-y-scroll hide-scrollbar pb-8">
         {loading ? (
           <PageLoader />
         ) : error ? (
